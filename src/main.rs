bf36b6db750221bb89b06f0f9cbf876eb80d158b
smabbasht <abbastaqvi81@gmail.com>
2024-06-03 06:07:08 -0700

    complete the challenge

--- src/main.rs
+++ src/main.rs
@@ -3,51 +3,65 @@ use std::{
     f32::INFINITY,
     fs::File,
     io::{BufRead, BufReader, BufWriter, Write},
+    time::Instant,
 };
 
 fn main() {
-    let datapath = "../data/measurements.txt";
+    let now = Instant::now();
+
+    let datapath = "data/measurements.txt";
 
     let file = File::open(datapath).expect("Panicked while opening file");
     let reader = BufReader::new(file);
 
     //let chunk_size = 1_000_000;
-    let mut data: HashMap<&str, Vec<f32>> = HashMap::new();
+    let mut data: HashMap<String, Vec<f32>> = HashMap::new();
 
+    let mut counter = 0;
     for line in reader.lines() {
+        counter += 1;
+        if counter % 1000 == 0 {
+            println!("Processing {} line", counter)
+        };
         let row = line.expect("Panicked while loading record");
-        let record: Vec<&str> = row.split(';').collect();
-        let city = record[0].clone();
-        let temperature: f32 = match record[1].clone().parse() {
-            Ok(v) => v,
-            Err(_) => 0.0,
-        };
+        if let Some((state, temperature)) = row.split_once(';') {
+            let temperature: f32 = match temperature.parse() {
+                Ok(v) => v,
+                Err(_) => 0.0,
+            };
 
-        let corr_entry = data.entry(city).or_insert(vec![INFINITY, 0.0, 0.0, 0.0]);
+            let corr_entry = data
+                .entry(state.to_string())
+                .or_insert(vec![INFINITY, 0.0, 0.0, 0.0]);
 
-        corr_entry[0] = if temperature < corr_entry[0] {
-            temperature
-        } else {
-            corr_entry[0]
-        };
-        corr_entry[1] += ((corr_entry[1] * corr_entry[3]) + temperature) / (corr_entry[3] + 1.0);
-        corr_entry[2] = if temperature > corr_entry[2] {
-            temperature
-        } else {
-            corr_entry[2]
-        };
-        corr_entry[3] += 1.0;
+            corr_entry[0] = if temperature < corr_entry[0] {
+                temperature
+            } else {
+                corr_entry[0]
+            };
+            corr_entry[1] +=
+                ((corr_entry[1] * corr_entry[3]) + temperature) / (corr_entry[3] + 1.0);
+            corr_entry[2] = if temperature > corr_entry[2] {
+                temperature
+            } else {
+                corr_entry[2]
+            };
+            corr_entry[3] += 1.0;
+        }
     }
 
     let outfile = File::create("output.txt").expect("Panicked while opening/creating outfile");
     let mut writer = BufWriter::new(outfile);
 
-    for (city, stats) in &data {
-        let outline = format!("{}={}/{}/{}, ", city, stats[0], stats[1], stats[2]);
+    for (state, stats) in &data {
+        let outline = format!("{}={}/{}/{}, ", state, stats[0], stats[1], stats[2]);
         writer
             .write_all(outline.as_bytes())
             .expect("Panicked while writing outline");
     }
 
     writer.flush().expect("Panicked while flushing the output");
+
+    let elapsed_time = now.elapsed();
+    println!("The code took {} seconds to run", elapsed_time.as_secs());
 }
