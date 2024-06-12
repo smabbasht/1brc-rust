1f87678ec42e870c41c271c0c708adc0881b7220
smabbasht <abbastaqvi81@gmail.com>
2024-06-12 17:18:38 +0500

    add multi threading using scoped threads

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,88 @@
-use std::{
-    collections::HashMap,
-    f32::INFINITY,
-    fs::File,
-    io::{BufRead, BufReader, BufWriter, Write},
-    time::Instant,
-};
+use std::collections::HashMap;
+use std::fs;
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::sync::{Arc, Mutex};
+use std::thread::available_parallelism;
+use std::time::Instant;
 
 fn main() {
     let now = Instant::now();
+    let n_threads = available_parallelism().unwrap().get();
 
-    let datapath = "data/measurements.txt";
+    // let datalines = fs::read_to_string("data/measurements_lite.txt").unwrap();
+    let datalines = fs::read_to_string("data/measurements.txt").unwrap();
+    let lines: Vec<&str> = datalines.lines().collect();
 
-    let file = File::open(datapath).expect("Panicked while opening file");
-    let reader = BufReader::new(file);
+    let n_lines = lines.len();
 
-    //let chunk_size = 1_000_000;
-    let mut data: HashMap<String, Vec<f32>> = HashMap::new();
+    let chunk_size = (n_lines) / n_threads;
+
+    let data_map: Arc<Mutex<HashMap<&str, [f32; 4]>>> = Arc::new(Mutex::new(HashMap::new()));
+    println!(
+        "Done doing init and readfile in {}s",
+        now.elapsed().as_secs()
+    );
 
     let mut counter = 0;
-    for line in reader.lines() {
-        counter += 1;
-        if counter % 1000 == 0 {
-            println!("Processing {} line", counter)
-        };
-        let row = line.expect("Panicked while loading record");
-        if let Some((state, temperature)) = row.split_once(';') {
-            let temperature: f32 = match temperature.parse() {
-                Ok(v) => v,
-                Err(_) => 0.0,
-            };
+    std::thread::scope(|s| {
+        for chunk in lines.chunks(chunk_size) {
+            counter += 1;
+            let data_map = Arc::clone(&data_map);
+            s.spawn(move || {
+                let now = Instant::now();
+                println!("Thread {} started", counter);
+                let chunk = chunk.to_vec();
+                let mut local_chunk: HashMap<&str, [f32; 4]> = HashMap::new();
 
-            let corr_entry = data
-                .entry(state.to_string())
-                .or_insert(vec![INFINITY, 0.0, 0.0, 0.0]);
+                for line in chunk {
+                    if let Some((state, temperature)) = line.split_once(';') {
+                        let temperature = temperature.parse().unwrap();
+                        let entry = local_chunk
+                            .entry(state)
+                            .or_insert([100.0, 0.0, -100.0, 0.0]);
+                        entry[0] = entry[0].min(temperature);
+                        entry[1] += temperature;
+                        entry[2] = entry[2].max(temperature);
+                        entry[3] += 1.0;
+                    }
+                }
 
-            corr_entry[0] = if temperature < corr_entry[0] {
-                temperature
-            } else {
-                corr_entry[0]
-            };
-            corr_entry[1] +=
-                ((corr_entry[1] * corr_entry[3]) + temperature) / (corr_entry[3] + 1.0);
-            corr_entry[2] = if temperature > corr_entry[2] {
-                temperature
-            } else {
-                corr_entry[2]
-            };
-            corr_entry[3] += 1.0;
+                let data = &mut data_map.lock().unwrap();
+                for (state, stats) in local_chunk {
+                    let entry = data.entry(state).or_insert([100.0, 0.0, -100.0, 0.0]);
+                    entry[0] = stats[0].min(entry[0]);
+                    entry[1] += stats[1];
+                    entry[2] = stats[2].max(entry[2]);
+                    entry[3] += stats[3];
+                }
+                println!(
+                    "Thread {} is ending in {}ms",
+                    counter,
+                    now.elapsed().as_millis()
+                );
+            });
         }
-    }
+    });
 
+    let now = Instant::now();
     let outfile = File::create("output.txt").expect("Panicked while opening/creating outfile");
     let mut writer = BufWriter::new(outfile);
+    let data = data_map.lock().unwrap();
 
-    for (state, stats) in &data {
-        let outline = format!("{}={}/{}/{}, ", state, stats[0], stats[1], stats[2]);
+    for (state, stats) in &*data {
+        let outline = format!(
+            "{}={}/{:.1}/{}, ",
+            state,
+            stats[0],
+            stats[1] / stats[3],
+            stats[2]
+        );
         writer
             .write_all(outline.as_bytes())
             .expect("Panicked while writing outline");
     }
 
     writer.flush().expect("Panicked while flushing the output");
-
-    let elapsed_time = now.elapsed();
-    println!("The code took {} seconds to run", elapsed_time.as_secs());
+    println!("Done writing to file in {}ms", now.elapsed().as_millis());
 }
